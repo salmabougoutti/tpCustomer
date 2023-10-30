e834decc883d683289287bdb06bfa78eded9aea1
Bougoutti <salmaboug5@gmail.com>
2023-10-30 13:28:58 +0100

    commit8

--- src/main/java/ma/emsi/tpcutomerbougoutti/service/CustomerManager.java
+++ src/main/java/ma/emsi/tpcutomerbougoutti/service/CustomerManager.java
@@ -34,4 +34,7 @@ public class CustomerManager {
     public void persist(Customer customer) {
         em.persist(customer);
     }
+    public Customer findById(int idCustomer) {  
+      return em.find(Customer.class, idCustomer);  
+}
 }
